368501eb65d3dfdaf192c1b77a38ba8a1b405366
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-07-15 05:27:48 +0000

    Fixes indentation for the previous commit

--- scripts/proccess_data.py
+++ scripts/proccess_data.py
@@ -22,9 +22,11 @@ def remove_outliers(df, threshold = 3, method='mean' )->pd.DataFrame:
     print('Outlier removals complete')
     return df
 
-    def create_data_partition(df, data_col_name, part_col_name, num_parts)->pd.DataFrame:
-        seg = df
-        print('Creating {} partions using data from {}...',format(num_parts,data_col_name))
-        seg[part_col_name] = pd.qcut(seg[data_col_name], num_parts,labels = False)
-        print('Done! Partions saved with column name {}',format(part_col_name))
-        return seg
+def create_data_partition(df, data_col_name, part_col_name, num_parts)->pd.DataFrame:
+    seg = df
+    print('Creating {} partions using data from {}...',format(num_parts,data_col_name))
+    seg[part_col_name] = pd.qcut(seg[data_col_name], num_parts,labels = False)
+    print('Done! Partions saved with column name {}',format(part_col_name))
+    return seg
+
+    

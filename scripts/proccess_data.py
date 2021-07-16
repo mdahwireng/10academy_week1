bdbdf12d8cabe357e6b9685b36c1934a7f7772b2
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-07-16 08:03:14 +0000

    Adds function to create pca analysis

--- scripts/proccess_data.py
+++ scripts/proccess_data.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy  import stats
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import math
 
 def remove_outliers(df, threshold = 3, method='mean' )->pd.DataFrame:
     print('Iterating through columns of dataframe...')
@@ -39,3 +42,40 @@ def group_and_sum(df, groupby_col_name, sumby_col_lst)->pd.DataFrame:
     outp = seg.groupby(groupby_col_name)[sumby_col_lst].sum()
     print('\nGrouping and aggregation completed.')
     return outp
+
+def create_pca(df, num_components)->pd.DataFrame:
+    raw_input =  df.copy()
+    sscaler = StandardScaler()
+
+    print('Scaling inputs...')
+    scaled_pca_input = sscaler.fit_transform(raw_input)
+
+    print('Initializing PCA...')
+    pca = PCA(n_components=num_components)
+    input_dict = {'scaled_input':scaled_pca_input, 'raw_input': raw_input }
+
+    print('Creating Principal Components for scaled and raw inputs...')
+    pca_dict = {i : {'pca_data':pca.fit_transform(input_dict[i]), 'pca':pca} for i in input_dict}
+
+    print('Scoring Principal Components of scaled and raw inputs...')
+    for i in pca_dict:
+        pca_dict[i]['score'] = pca_dict[i]['pca'].explained_variance_ratio_.sum()
+
+    print('Selecting the Principal Components with the best score...')
+    score = 0
+    for i in pca_dict:
+        if pca_dict[i]['score'] > score:
+            pca_dict[i]['name']= i
+            selected_pca = [pca_dict[i]]
+
+    print('\nThe selected Principal components retains {}% of the data with a lesser number of features on {}'
+        .format(math.trunc(selected_pca[0]['score']*100), selected_pca[0]['name']))
+    
+    print('Creating dataframe for selected Principal Components')
+    columns = ['principal component ' + str(i+1) for i in range(num_components)]
+    
+    componentDf = pd.DataFrame(data = selected_pca[0]['pca_data']
+                , columns = columns)
+    print('\nProcesses completed and dataframe for selected Principal Components returned')
+    
+    return componentDf
